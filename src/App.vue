5bf823ac0645dcb881e67a7a1b4c7e8214409083
Plantere <bfabiobenfica@gmail.com>
2022-06-30 19:09:46 -0300

    added: header and footer in main component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
 	<div id="app">
-		<SensorProject />
+		<header class="header">
+			<b class="title-page"> PROJETO "TERRA MOIADA" - 2022/01</b>
+		</header>
+		<SensorProject class="sensor-project" />
+		<footer>
+			<p class="text-white">Projeto desenvolvido por: Matheus, Rafael, Thiago</p>
+		</footer>
 	</div>
 </template>
 
@@ -16,12 +22,35 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@700&display=swap');
+
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
+
+	background-color: #a7c957;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	overflow: hidden;
+	z-index: -1;
+}
+
+.header {
+	background-color: #386641;
+	padding: 60px;
+	margin-bottom: 60px;
+}
+
+.title-page {
+	color: white;
+	font-size: 4rem;
+	text-align: left;
+	font-family: 'Rajdhani', sans-serif;
 }
 </style>
