14ba04fc459a865adb39474f92d49682c1328110
Plantere <bfabiobenfica@gmail.com>
2022-06-30 20:56:44 -0300

    added: title in page and change favicon

--- src/App.vue
+++ src/App.vue
@@ -61,6 +61,9 @@ export default {
 			this.config.modal = true;
 		},
 	},
+	created() {
+		document.title = 'Projeto "Terra Moiada" ';
+	},
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
