5aff3d06afd7ffe60ac35f629ffafbfeb4f0fbf4
Fábio Torres <36510131+Plantere@users.noreply.github.com>
2022-10-12 12:52:12 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const firebaseConfig = {
-	apiKey: 'psxF8qHwBEX9bziGWfAUWUHqB7ubEMmgQ9Z4lKA8',
-	authDomain: 'soil-moisture-monitoring-7e2bb.firebaseapp.com',
-	databaseURL: 'https://soil-moisture-monitoring-7e2bb-default-rtdb.firebaseio.com',
-	projectId: 'soil-moisture-monitoring-7e2bb',
-	storageBucket: 'soil-moisture-monitoring-7e2bb.appspot.com',
-	messagingSenderId: '652449505450',
-	appId: '1:652449505450:web:85001e5091d018dc5b70d3',
+	apiKey: "AIzaSyDdGK1NrAm1Fng3nU6pRMa_47xRxHM6f6I",
+	authDomain: "soil-moisture-monitoring-7e2bb.firebaseapp.com",
+	databaseURL: "https://soil-moisture-monitoring-7e2bb-default-rtdb.firebaseio.com/",
+	projectId: "soil-moisture-monitoring-7e2bb",
+	storageBucket: "soil-moisture-monitoring-7e2bb.appspot.com",
+	messagingSenderId: "652449505450",
+	appId: "1:652449505450:web:85001e5091d018dc5b70d3"
 };
 
 firebase.initializeApp(firebaseConfig);
