bcc79bc86e1286ac7696dcec31d479f4e996cd2f
kroonus2 <rjardimc3@gmail.com>
2022-11-27 16:57:42 -0300

    versão  - 1.1

---
+++ src/components/BombControl.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <b-container class="bv-example-row">
+            <b-row>
+                <b-col>
+                    <b-input-group style="display:block;">
+                        <b-button class=" btn btn-success" @click="lessSlideControl()">
+                            <b-icon icon="dash-lg"></b-icon>
+                        </b-button>
+                        <input type="text" class="text-center" v-model="lastSlide" style="height: 36px;" disabled>
+                        <b-button class="btn btn-success" @click="plusSlideControl()">
+                            <b-icon icon="plus-lg"></b-icon>
+                        </b-button>
+                    </b-input-group>
+                </b-col>
+                <div class="w-100"></div>
+                <b-col>
+                    <b-button class="p-2 my-3" :class="[lastStatus ? 'btn-danger' : 'btn-success']"
+                        @click="toggleStatus()">
+                        {{ lastStatus ? 'DESLIGAR' : 'LIGAR' }}
+                    </b-button>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+
+</template>
+
+<script>
+import FirebaseServices from '../services/FirebaseServices.js';
+
+export default {
+    data() {
+        return {
+            controlStatus: false,
+            controlSlide: 0,
+        }
+    },
+
+    computed: {
+        lastStatus() {
+            return this.controlStatus;
+        },
+
+        lastSlide() {
+            return this.controlSlide;
+        },
+
+        lastNotStatus() {
+            return this.uBombControl;
+        },
+
+    },
+    methods: {
+        getSelectedDataBombControl() {
+            FirebaseServices.getStateBombControl().on('value', (Control) => {
+                let statusControl = Control.val();
+                this.controlStatus = statusControl;
+            })
+        },
+
+        getSelectedDataSlideControl() {
+            FirebaseServices.getValueSlideControl().on('value', (Slide) => {
+                let slideControl = Slide.val();
+                this.controlSlide = slideControl;
+            })
+        },
+
+        toggleStatus() {
+            FirebaseServices.getStateBombControl().set(!this.lastStatus);
+        },
+
+        plusSlideControl() {
+            if (this.controlSlide >= 100) return
+            FirebaseServices.getValueSlideControl().set(this.controlSlide += 1);
+        },
+
+        lessSlideControl() {
+            FirebaseServices.getValueSlideControl().set(this.controlSlide -= 1);
+        }
+
+    },
+
+    created() {
+        this.getSelectedDataBombControl();
+        this.getSelectedDataSlideControl();
+    },
+}
+</script>
--- src/services/FirebaseServices.js
+++ src/services/FirebaseServices.js
@@ -2,6 +2,8 @@ import firebase from '../firebase';
 
 const databaseTemperature = firebase.ref('/_DHT11/Temperatura');
 const databaseSoilMoisture = firebase.ref('/_SoilMoisture/Umidade Solo Porcentagem');
+const databaseSlideControl = firebase.ref('/SlideControl');
+const databaseBombControl = firebase.ref('/BombControl');
 
 export default {
 	getAllTemperature() {
@@ -9,5 +11,11 @@ export default {
 	},
 	getAllSoilMoisture() {
 		return databaseSoilMoisture;
+	},
+	getValueSlideControl() {
+		return databaseSlideControl;
+	},
+	getStateBombControl() {
+		return databaseBombControl;
 	},
 };
